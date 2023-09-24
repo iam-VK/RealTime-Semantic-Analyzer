8db8eaf140f791da048613897ed4cc5c7cb200a4
VK <vishwakumar.petit@gmail.com>
2023-09-24 12:19:00 +0530

    Basic web page and offline model config

--- API.py
+++ API.py
@@ -1,17 +1,14 @@
-from flask import Flask,request
+from flask import Flask
+from flask_cors import CORS
 from model import classifier
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/classify/<user_input>")
 def classify(user_input):
     model=classifier()
     return model.model_result(user_input)
 
-
-
-
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
-
--- model.py
+++ model.py
@@ -1,15 +1,19 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification,DistilBertForSequenceClassification, DistilBertTokenizer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoConfig
 from torch import nn
 import json
 
-model_name = "j-hartmann/emotion-english-distilroberta-base"#"SamLowe/roberta-base-go_emotions"#"cardiffnlp/twitter-roberta-base-sentiment"
+model_name = "roberta-base-go_emotions"
+#"SamLowe/roberta-base-go_emotions"
+#"j-hartmann/emotion-english-distilroberta-base"
+#"cardiffnlp/twitter-roberta-base-sentiment"
 ptmodel = AutoModelForSequenceClassification.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+config = AutoConfig.from_pretrained(model_name)
+#ptmodel.save_pretrained("./local_model")
 
 class classifier:
     def runmodel(self,sentence):
         user_input = sentence
-        print(user_input)
         ptbatch = tokenizer(user_input,padding=True,truncation=True,max_length=512,return_tensors="pt")
         ptoutputs = ptmodel(**ptbatch)
         ptprediction = nn.functional.softmax(ptoutputs.logits, dim=-1)
@@ -17,17 +21,16 @@ class classifier:
     
     def model_result(self,sentence):
         prediction=self.runmodel(sentence)
-        result_dict={
-            "Anger":prediction[0][0].item(),
-            "Disgust":prediction[0][1].item(),
-            "Fear":prediction[0][2].item(),
-            "Joy":prediction[0][3].item(),
-            "Neutral":prediction[0][4].item(),
-            "Sadness":prediction[0][5].item(),
-            "Surprise":prediction[0][6].item()
-        }
+        # result_dict={
+        #     "Anger":prediction[0][0].item(),
+        #     "Disgust":prediction[0][1].item(),
+        #     "Fear":prediction[0][2].item(),
+        #     "Joy":prediction[0][3].item(),
+        #     "Neutral":prediction[0][4].item(),
+        #     "Sadness":prediction[0][5].item(),
+        #     "Surprise":prediction[0][6].item()
+        # }
 
-        result_dict = sorted(result_dict.items(), key=lambda x:x[1],reverse=True)
-        result_json=json.dumps(result_dict,indent=2)
-        return result_json
-
+        # result_dict = sorted(result_dict.items(), key=lambda x:x[1],reverse=True)
+        # result_json=json.dumps(result_dict,indent=2)
+        return str(prediction)
